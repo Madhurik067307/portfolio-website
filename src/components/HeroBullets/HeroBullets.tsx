3be22a0c5d01ba07427f178779154e10d2089808
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-12-13 20:01:27 -0600

    second commit

--- src/components/HeroBullets/HeroBullets.tsx
+++ src/components/HeroBullets/HeroBullets.tsx
@@ -1,9 +1,19 @@
-import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
-import { useRouter } from 'next/navigation';
+import {
+  Image,
+  Container,
+  Title,
+  Button,
+  Group,
+  Text,
+  List,
+  ThemeIcon,
+  rem,
+} from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
 // import image from '../../assets/images/image.svg';
-import femaleTailor from '../../assets/images/female-tailor.png';
-import classes from './HeroBullets.module.css';
+import femaleTailor from "../../assets/images/female-tailor.png";
+import classes from "./HeroBullets.module.css";
 
 export function HeroBullets() {
   const router = useRouter();
@@ -13,7 +23,8 @@ export function HeroBullets() {
       <div className={classes.inner}>
         <div className={classes.content}>
           <Title className={classes.title}>
-            Tailor with <span className={classes.highlight}>10+</span> <br /> Years of Experience
+            Tailor with <span className={classes.highlight}>10+</span> <br />{" "}
+            Years of Experience
           </Title>
           {/* <Text c="dimmed" mt="md">
             Build fully functional accessible web applications faster than ever – Mantine includes
@@ -26,25 +37,28 @@ export function HeroBullets() {
             size="sm"
             icon={
               <ThemeIcon size={20} radius="xl">
-                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                <IconCheck
+                  style={{ width: rem(12), height: rem(12) }}
+                  stroke={1.5}
+                />
               </ThemeIcon>
             }
           >
             <List.Item>
-              <b>Tailoring Excellence</b> – Crafting garments with precision and passion for 10+
-              years.
+              <b>Tailoring Excellence</b> – Crafting garments with precision and
+              passion for 10+ years.
             </List.Item>
             <List.Item>
-              <b>Satisfaction Guaranteed</b> – We're dedicated to your satisfaction; your confidence
-              in our work is our top priority.
+              <b>Satisfaction Guaranteed</b> – We&apos;re dedicated to your
+              satisfaction; your confidence in our work is our top priority.
             </List.Item>
             <List.Item>
-              <b>Consultation & Styling</b> – Our expert stylists will guide you through the design
-              process, ensuring you look your best.
+              <b>Consultation & Styling</b> – Our expert stylists will guide you
+              through the design process, ensuring you look your best.
             </List.Item>
             <List.Item>
-              <b>Local and Family-Owned</b> – A locally owned and family-operated tailor shop,
-              dedicated to our community.
+              <b>Local and Family-Owned</b> – A locally owned and
+              family-operated tailor shop, dedicated to our community.
             </List.Item>
           </List>
 
@@ -53,7 +67,7 @@ export function HeroBullets() {
               radius="xl"
               size="md"
               className={classes.control}
-              onClick={() => router.push('/services')}
+              onClick={() => router.push("/services")}
             >
               Our Services
             </Button>
