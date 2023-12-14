121a471486be0f395fc54912e94d258f0dec7dde
Chuong Tran <chuongtran@Chuongs-MacBook-Pro.local>
2023-12-13 19:54:40 -0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Header } from "@/components/Header/Header";
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
---
+++ src/app/services/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Carousel } from "@/components/Carousel/Carousel";
+import { Header } from "@/components/Header/Header";
+import React from "react";
+
+export default function Services() {
+  return (
+    <>
+      <Header />
+      <Carousel title="Services" />
+    </>
+  );
+}
